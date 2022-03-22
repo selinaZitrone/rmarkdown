421d203bc3fa8f86a4093b32a55617aed315c1ab
Selina Baldauf <selina.baldauf@gmx.de>
2022-03-22 21:51:40 +0100

    First version of day 1

---
+++ R/Slides/day1_slides.R
@@ -0,0 +1,22 @@
+# Code for slides from day 3
+# Author: Selina Baldauf (selina.baldauf@fu-berlin.de)
+# Hint: navigate the script by opening the document outline (top right corner, next to Source button)
+
+# 1. R as calculator ---------------------------------------------------------
+
+# Arithmetic operators ----------------------------------------------------
+
+# Addition
+2 + 2
+# Subtraction
+5.432 - 34234
+# Multiplication
+33 * 42
+# Division
+3 / 42
+# Modulo (Remainder)
+2 %% 2
+# Power
+2^2
+# Combine operations
+((2 + 2) * 5)^(10 %% 10)

a5043e96a79007c65bbba74d853161de2ebaec7a
Selina Baldauf <selina.baldauf@gmx.de>
2022-09-12 13:35:38 +0200

    re-render site

--- docs/R/pipeline.R
+++ docs/R/pipeline.R
@@ -16,5 +16,4 @@ rmarkdown::render_site(encoding = "UTF-8")
 git2r::add(path = here::here("docs"))
 
 git2r::commit(message = "re-render site")
-git2r::push(credentials = git2r::cred_token())
-
+gert::git_push()
